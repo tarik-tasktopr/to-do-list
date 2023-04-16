7157203bf0da584ee7531f5f8864ed0b997aa078
TarikTaskToPR <tarik.tasktopr@gmail.com>
2023-04-16 19:21:01 +0200

    Create /app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskForm = document.getElementById('task-form');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    taskForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const taskText = taskInput.value.trim();
+
+        if (taskText) {
+            addTask(taskText);
+            taskInput.value = '';
+        }
+    });
+
+    function addTask(taskText) {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        taskList.appendChild(li);
+    }
+});
