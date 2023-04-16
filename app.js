acc522e8377c1d5e9690eeab58e7e5c6ed9de8ad
TarikTaskToPR <tarik.tasktopr@gmail.com>
2023-04-16 19:02:15 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTasksFromLocalStorage() {
+  const tasks = localStorage.getItem('tasks');
+  return tasks ? JSON.parse(tasks) : [];
+}
+
+function addTaskToList(task) {
+  const listItem = document.createElement('li');
+  listItem.textContent = task;
+  taskList.appendChild(listItem);
+}
+
+function loadTasks() {
+  const tasks = getTasksFromLocalStorage();
+  tasks.forEach(addTaskToList);
+}
+
+function addTask(event) {
+  event.preventDefault();
+  const task = taskInput.value.trim();
+
+  if (task) {
+    addTaskToList(task);
+    const tasks = getTasksFromLocalStorage();
+    tasks.push(task);
+    saveTasksToLocalStorage(tasks);
+    taskInput.value = '';
+  }
+}
+
+taskForm.addEventListener('submit', addTask);
+loadTasks();
